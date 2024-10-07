d94374355710d3c5ff0b876bdf15aac05cd532c6
fakersl2 <projetotccfigma@outlook.com>
2024-10-07 10:34:53 -0300

    Criando o componente Principal
    Co-Authored-By: fakersl <84857762+fakersl@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'principal',
+        pathMatch: 'full',
+        component: PrincipalComponent,
+    }
+];
